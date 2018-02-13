2cbae94849f71517ef810685c6eaed167896461f
Yonatan Miller <brombacher1@gmail.com>
2018-02-13 23:03:47 +0100

    Add before_update filter for User#emails and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
 
   has_many :previous_emails, dependent: :destroy
 
+  before_update :persist_user_emails, if: :will_save_change_to_email?
   validates_uniqueness_of :email
 
+  def persist_user_emails
+    previous_emails.build(email: email_before_last_save)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,52 @@
+# frozen_string_literal: true
+
 describe User do
-
-  before(:each) { @user = User.new(email: 'user@example.com') }
-
-  subject { @user }
+  let(:user) { create(:user, email: 'email@1.com') }
 
   it { should respond_to(:email) }
 
-  it "#email returns a string" do
-    expect(@user.email).to match 'user@example.com'
+  it '#email returns a string' do
+    expect(user.email).to match 'email@1.com'
   end
 
+  def new_updated_email(*numbers)
+    numbers.each do |n|
+      user.email = "email@#{n}.com"
+      user.save
+    end
+  end
+
+  context 'when updating single email' do
+    it 'persists changed email' do
+      expect(user).to receive(:persist_user_emails).once
+
+      new_updated_email(2)
+    end
+
+    it 'does not persist unchanged email' do
+      expect(user).to_not receive(:persist_user_emails)
+
+      new_updated_email(1)
+    end
+  end
+
+  context 'when updating unique emails' do
+    it 'persist two changed emails' do
+      expect(user).to receive(:persist_user_emails).twice
+
+      new_updated_email(2, 3)
+    end
+
+    it 'persist emails when changing back to original email' do
+      expect(user).to receive(:persist_user_emails).twice
+
+      new_updated_email(2, 1)
+    end
+
+    it 'persist only unique emails' do
+      expect(user).to receive(:persist_user_emails).twice
+
+      new_updated_email(1, 2, 1)
+    end
+  end
 end
